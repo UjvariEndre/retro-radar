0dda0786a9a247f6681376dce62c418f7f025059
UjvariEndre <ujvendre@gmail.com>
2025-03-27 13:27:18 +0800

    refactor: :recycle: Remove per page select.

--- components/search/SearchBar.tsx
+++ components/search/SearchBar.tsx
@@ -1,23 +1,13 @@
 "use client";
 
 import { useFilters } from "@/hooks/useFilters";
-import { SelectOptionsModel } from "@/lib/models/ui.models";
 import { useRouter } from "next/navigation";
 import { ChangeEvent, useEffect, useState } from "react";
 import RRInput from "../features/RRInput";
-import RRSelect from "../features/RRSelect";
-
-const OPTIONS: SelectOptionsModel = [
-  { value: "15", title: "15 Results Per Page" },
-  { value: "30", title: "30 Results Per Page" },
-  { value: "50", title: "50 Results Per Page" },
-  { value: "100", title: "100 Results Per Page" },
-];
 
 export default function SearchBar() {
   const router = useRouter();
-  const { keyword, pageSize, setKeyword, setPageSize, setPageIndex } =
-    useFilters();
+  const { keyword, setKeyword, setPageIndex } = useFilters();
   const [debouncedKeyword, setDebouncedKeyword] = useState(keyword);
 
   // Debounce effect
@@ -38,25 +28,14 @@ export default function SearchBar() {
     setPageIndex(0);
   };
 
-  const handlePageSizeChange = (value: string | undefined) => {
-    setPageSize(Number(value));
-    setPageIndex(0);
-  };
-
   return (
     <form className="mb-6 flex justify-center space-x-1">
       <RRInput
         type="text"
         value={debouncedKeyword}
         onChange={(e) => handleSearch(e)}
-        placeholder="Search games..."
+        placeholder="Search game titles..."
         className="max-w-96"
-      />
-      <RRSelect
-        options={OPTIONS}
-        onChange={handlePageSizeChange}
-        value={pageSize.toString()}
-        className="w-[200px]"
       />
     </form>
   );
